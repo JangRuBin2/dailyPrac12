a2ee54f48b6117b69fed9adcc4c625acfd9e5727
choboruru <wkdfnqls6806@gmail.com>
2023-05-06 13:33:00 +0900

    초기 조건

---
+++ jangrubin12.js
@@ -0,0 +1,13 @@
+const add = require('./utility-module/add.js');
+const minus = require('./utility-module/minus.js');
+const divide = require('./utility-module/divide.js');
+const multiply = require('./utility-module/multiply.js');
+
+const a = 10;
+const b = 20;
+
+console.log(add(a,b));
+console.log(minus(a,b));
+console.log(divide(a,b));
+console.log(multiply(a,b));
+
